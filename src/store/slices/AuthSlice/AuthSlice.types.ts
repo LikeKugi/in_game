a0eee60475552c03b5a33142df11a72ee7afee82
Ilya Shcherbakov <86749581+LikeKugi@users.noreply.github.com>
2024-02-06 19:06:09 +0300

    feat: add preloaded state

--- src/store/slices/AuthSlice/AuthSlice.types.ts
+++ src/store/slices/AuthSlice/AuthSlice.types.ts
@@ -1,6 +1,6 @@
 export interface IAuthSliceState {
-  "id": number| null,
-  "username": "string"| null,
-  "accessToken": "string"| null,
-  "refreshToken": "string"| null
+  'id': number | null,
+  'username': string | null,
+  'accessToken': string | null,
+  'refreshToken': string | null
 }
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,11 +1,23 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { RootReducer } from '@/store/RootReducer';
 import { api } from '@/api';
+import { useStorage } from '@/utils/useLocalStorage';
+
+// eslint-disable-next-line react-hooks/rules-of-hooks
+const {getTokenFromLocalStorage} = useStorage();
+const token = getTokenFromLocalStorage();
 
 export const store = configureStore({
   reducer: RootReducer,
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(api.middleware),
-  preloadedState: {}
+  preloadedState: {
+    AuthSliceReducer: {
+      refreshToken: token,
+      username: null,
+      accessToken: null,
+      id: null,
+    }
+  }
 });
 
 export type RootState = ReturnType<typeof store.getState>
