ff2a41e33b46ad1444013dba34db35a8d6c6c2c4
Ilya Shcherbakov <86749581+LikeKugi@users.noreply.github.com>
2024-02-22 17:55:42 +0300

    fix: fix menu on small size

--- src/pages/RootPage/RootPage.tsx
+++ src/pages/RootPage/RootPage.tsx
@@ -1,5 +1,5 @@
 import { JSX, useState } from 'react';
-import { Layout, Skeleton, theme } from 'antd';
+import { Drawer, FloatButton, Layout, Skeleton, theme } from 'antd';
 import Sider from 'antd/es/layout/Sider';
 import { Content, Header, Footer } from 'antd/es/layout/layout';
 import AppFooter from '@/components/AppFooter/AppFooter';
@@ -12,10 +12,13 @@ import { useRefreshToken } from '@/hooks/useRefreshToken';
 import { useAppSelector } from '@/store/hooks';
 import { selectIsLoggedIn } from '@/store/slices/AuthSlice';
 import { selectUserRole } from '@/store/slices/RolesSlice';
+import { AiOutlineMenuFold } from 'react-icons/ai';
 
 const RootPage = (): JSX.Element => {
 
   const [isCollapsed, setIsCollapsed] = useState(true);
+  const [isOpenedDrawer, setIsOpenedDrawer] = useState(false);
+
   const screens = useBreakpoint();
 
   const { isLoading } = useRefreshToken();
@@ -27,27 +30,50 @@ const RootPage = (): JSX.Element => {
   const isLoggedIn = useAppSelector(selectIsLoggedIn);
   const userRole = useAppSelector(selectUserRole);
 
+  const onCloseDrawer = () => {
+    setIsOpenedDrawer(false);
+  };
+
+  const toggleDrawer = () => {
+    setIsOpenedDrawer(prevState => !prevState);
+  };
+
   return (
 
     <Layout className={styles.RootPage}>
-      <Sider collapsible
-             collapsed={isCollapsed}
-             collapsedWidth={!screens.md ? 0 : undefined}
-             onCollapse={() => setIsCollapsed(prevState => !prevState)}
-             theme="dark"
-             zeroWidthTriggerStyle={{
-               top: '0.7rem',
-             }}>
+      {screens.md && (<Sider collapsible
+                             collapsed={isCollapsed}
+                             collapsedWidth={!screens.md ? 0 : undefined}
+                             onCollapse={() => setIsCollapsed(prevState => !prevState)}
+                             theme="dark"
+                             zeroWidthTriggerStyle={{
+                               top: '0.7rem',
+                             }}>
         <Skeleton loading={isLoading}>
-          <AppMenu isLoggedIn={isLoggedIn} userRole={userRole}/>
+          <AppMenu isLoggedIn={isLoggedIn}
+                   userRole={userRole}/>
         </Skeleton>
-      </Sider>
+      </Sider>)}
+
       <Content style={{ display: 'flex', flexDirection: 'column' }}>
         <Header style={{ backgroundColor: colorBgContainer }}>
 
         </Header>
         <Layout style={{ display: 'flex' }}>
           <Content>
+            {!screens.md && (<>
+              <Drawer title={'Главное меню'}
+                      placement={'right'}
+                      size="default"
+                      open={isOpenedDrawer}
+                      onClose={onCloseDrawer}>
+                <Skeleton loading={isLoading}>
+                  <AppMenu isLoggedIn={isLoggedIn}
+                           userRole={userRole}/>
+                </Skeleton>
+              </Drawer>
+              <FloatButton onClick={toggleDrawer} tooltip={<div>Меню</div>} icon={<AiOutlineMenuFold/>} />;
+            </>)}
             <Skeleton loading={isLoading}>
               <Outlet/>
             </Skeleton>
